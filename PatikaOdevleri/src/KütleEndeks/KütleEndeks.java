d82e3ef61179e575d6c048363572adbac5a4d440
Berat Agaca <berat0925@outlook.com>
2022-10-31 07:49:05 +0300

    try

--- PatikaOdevleri/src/KütleEndeks/KütleEndeks.java
+++ PatikaOdevleri/src/KütleEndeks/KütleEndeks.java
@@ -1,10 +1,19 @@
 package KütleEndeks;
 
+import java.util.Scanner;
+
 public class KütleEndeks {
 
 	public static void main(String[] args) {
+		float boy=0, kilo=0;
 		
-
+		Scanner getData = new Scanner(System.in);
+		System.out.print("Kilonuzu giriniz: ");
+		kilo = getData.nextFloat();
+		System.out.println("Boyunuzu(metre cinsinden) giriniz: ");
+		boy = getData.nextFloat();
+		System.out.println("Vücut Kütle endeksiniz: "+ kilo/(boy*boy));
+		
 	}
 
 }
--- PatikaOdevleri/src/Manav/Manav.java
+++ PatikaOdevleri/src/Manav/Manav.java
@@ -1,9 +1,36 @@
 package Manav;
 
+import java.util.Scanner;
+
 public class Manav {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double armut=2.14;
+		double elma=3.67;
+		double domates= 1.11;
+		double muz= 0.95;
+		double patlican=5.00;
+		float total=0, kilo=0;
+		Scanner getData = new Scanner(System.in);
+		
+		System.out.println("armut Kaç Kilo ?");
+		kilo= getData.nextFloat();
+		total += kilo*armut;
+		System.out.println("elma Kaç Kilo ?");
+		kilo= getData.nextFloat();
+		total += kilo*elma;
+		System.out.println("domates Kaç Kilo ?");
+		kilo= getData.nextFloat();
+		total += kilo*domates;
+		System.out.println("muz Kaç Kilo ?");
+		kilo= getData.nextFloat();
+		total += kilo*muz;
+		System.out.println("patlican Kaç Kilo ?");
+		kilo= getData.nextFloat();
+		total += kilo*patlican;
+		
+		System.out.println("Toplam Tutar : "+total+ " TL");
+		
 
 	}
 
